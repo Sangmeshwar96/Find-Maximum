4ad08884d0fc784d4a3b2ffe78aeee85eea4b987
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-12 17:02:55 +0530

    Finding Maximum Values Using Generics

--- Program.cs
+++ Program.cs
@@ -8,8 +8,15 @@ namespace Generic
         static void Main(string[] args)
         {
             Console.WriteLine("Find Maximum of Numbers ");
-            int maxNum = FindMaxNumber.FindMax(20, 10, 20);
+            //int maxNum = FindMaxNumber.FindMax(20, 10, 20);
+            //float maxNum = FindMaxNumber.FindMax(0.2f, 0.1f, 0.0f);
+            //string maxNum = FindMaxNumber.FindMax("Apple", "Banana", "Mango");
+            string maxNum = FindMaxNumber.FindMax<string>("Apple", "Banana", "Peach");
             Console.WriteLine(maxNum);
+
+            int maxNumInInt = FindMaxNumber.FindMax<int>(10, 20, 30);
+            Console.WriteLine(maxNumInInt);
+            
         }
     }
 }
