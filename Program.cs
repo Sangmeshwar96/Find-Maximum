ba5b2c318b4326d6bd561e0780d45d541ecb37ee
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-12 17:26:21 +0530

    Finding Maximum String Value Among Three Values

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using FindMaximumValue;
+using System;
+
+namespace Generic
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Generics Program To Find The Maximum Values");
+            Console.WriteLine(" ");
+            FindMax check = new FindMax();
+            //check.ToCompare(3, 2, 1);
+            //check.ToCompare(3.2f, 2.1f, 1.9f);
+            check.ToCompare("Patil", "Sangmeshwar", "Ramesh");
+
+        }
+    }
+}
