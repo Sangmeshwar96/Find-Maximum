1e23fa145df0e2bc2a50751060f15572879accd1
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-12 16:49:35 +0530

    Add files via upload
    [Sangmeshwar Patil]:Finding Max Int Number

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Generic
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Maximum of Numbers ");
+            int maxNum = FindMaxNumber.FindMax(20, 10, 20);
+            Console.WriteLine(maxNum);
+        }
+    }
+}
